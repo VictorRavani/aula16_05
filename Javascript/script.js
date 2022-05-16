3e1070d98de21f03fa7f65dc7d81b9dfe2b68a7b
VictorRavani <victorravani98@gmail.com>
2022-05-16 20:47:24 -0300

    Para salvar arquivos

---
+++ Javascript/script.js
@@ -0,0 +1,71 @@
+/*alert("Seja Bem Vindo");
+document.write("TESTE");
+prompt("Digite Algo:");
+resp = confirm("SIM");
+Nome ="Maria";
+var Nome1 ="jose";
+var a,b;
+a = 5;
+b ="5Kg de açucar";
+document.write("O valor da variárevel é:" + a);
+document.write("<p>"+"O valor da variável B é:" + b + "<p>");
+document.write("<p>"+"O valor da variável A dividido por 2 é:" + a/2 + "<p>");
+document.write("<p>"+"O valor da variável B dividido por 2 é:" + b/2 +"<p>");
+
+var c, interiro, flutuante;
+c = "45.78 Kg de açucar";
+interiro = parseInt(c);
+flutuante = parseFloat(c);
+document.write("O valor da variárevel é:" + c);
+document.write("<p>" + "O valor da variável B é:" + interiro + "<p>");
+document.write("<p>" + "O valor da variável A dividido por 2 é:" + interiro / 2 + "<p>");
+document.write("<p>" + "O valor da variável B dividido por 2 é:" + flutuante + "<p>");
+
+a = 6, b = 3, c = 1;
+document.write((a > b) && (b > c));
+
+a = 6, b = 3, c = 1;
+alert((a > b) || (b == c));
+
+a = 6, b = 3, c = 1;
+alert(a != b);
+
+*/
+//resp = confirm("Próximo dia?");
+
+//for (xDia = 0; xDia < 7; xDia ++)
+//{
+    
+//hoje = new Date();
+//diaSemana = new Array(7);
+//diaSemana[0] = "Frango Frito";
+//diaSemana[1] = "Bife a Rolê";
+//diaSemana[2] = "Panqueca";
+//diaSemana[3] = "Macarrão";
+//diaSemana[4] = "Churrasco";
+//diaSemana[5] = "Saladão";
+//diaSemana[6] = "Peixe frito";
+//document.write("o número da semana é:" + " " + (xDia + 1));
+//document.write("<p><b>" + "hoje é:" + diaSemana[xDia]);
+
+//diaSemana = new Array(7);
+//diaSemana[0] = "R$ 20,00";
+//diaSemana[1] = "R$ 35,00";
+//diaSemana[2] = "R$ 32,00";
+//diaSemana[3] = "R$ 62,30";
+//diaSemana[4] = "R$ 95,00";
+//diaSemana[5] = "R$ 13,00";
+//diaSemana[6] = "R$ 68,00";
+//document.write("<p><b>" + "Preço:" + diaSemana[xDia]);
+//alert("Seja Bem Vindo");
+
+//}
+
+
+a= 5;
+alert(++a +2);
+alert("a=" + a);
+
+a=5;
+alert(a++ + 2);
+alert("a="+ a);
